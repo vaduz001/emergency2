0f812761d3923e02ca8f203b9d6157c051092971
vaduz001 <vaduz001@gmail.com>
2016-04-06 00:17:25 -0400

    EJS FIX
    Update 2 0.55
    - Mongo Working
    - Pre Render

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/myappdatabase');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+/* GET *Userlist* database page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
 
 module.exports = router;
